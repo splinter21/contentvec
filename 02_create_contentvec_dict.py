8016d2931684161aa63439ed0b77f3c21fbea307
bfloat16 <a1004428886@126.com>
2024-07-17 16:17:39 +0800

    fix 02

--- 02_create_contentvec_dict.py
+++ 02_create_contentvec_dict.py
@@ -16,14 +16,14 @@ def extract_embedding(filepath, encoder):
     embedding = torch.tensor(file_embedding)
     return embedding
 
-def process_files(filelist, root_folder, device):
-    torch.cuda.set_device(device)
+def process_files(rank, filenames, root_folder, device_id, return_dict):
+    torch.cuda.set_device(device_id)
     encoder = VoiceEncoder()
-    fcpe = spawn_bundled_infer_model(device=device)
+    fcpe = spawn_bundled_infer_model(device=device_id)
     
     def get_f0_with_fcpe(filepath):
         audio, sr = librosa.load(filepath, sr=None, mono=True)
-        _audio = torch.from_numpy(audio).to(device).unsqueeze(0)
+        _audio = torch.from_numpy(audio).to(device_id).unsqueeze(0)
         f0 = fcpe(_audio, sr=sr, decoder_mode="local_argmax", threshold=0.006)
         f0 = f0.squeeze().cpu().numpy()
         f0_p = f0[f0 > 0]
@@ -31,11 +31,11 @@ def process_files(filelist, root_folder, device):
     
     speaker_dict = {}
     
-    for filepath in tqdm(filelist):        
+    for filepath in tqdm(filenames):        
         speaker_id = str(filepath)
         filepath = join(root_folder, filepath)
         if not exists(filepath):
-            print("file {} doesn't exist!".format(filepath))
+            print(f"file {filepath} doesn't exist!")
             continue
 
         embedding = extract_embedding(filepath, encoder=encoder)
@@ -45,38 +45,39 @@ def process_files(filelist, root_folder, device):
             print(f"Error: {filepath}: {e}")
             continue
         speaker_dict[speaker_id] = embedding.numpy(), (f0_min, f0_max, f0_mean)
-    return speaker_dict
+    
+    return_dict[rank] = speaker_dict
 
-def worker_init(rank, filenames, root_folder, device_id, result_queue):
-    result = process_files(filenames, root_folder, device_id)
-    result_queue.put(result)
-
-def parallel_process(filenames, root_folder, num_processes, num_devices):
-    result_queue = mp.Queue()
+def parallel_process(filenames, root_folder, num_processes):
+    mp.set_start_method('spawn', force=True)
+    manager = mp.Manager()
+    return_dict = manager.dict()
+    num_devices = torch.cuda.device_count()
     chunk_size = len(filenames) // num_processes
-    processes = []
 
+    processes = []
     for i in range(num_processes):
         start = i * chunk_size
         end = None if i == num_processes - 1 else (i + 1) * chunk_size
         file_chunk = filenames[start:end]
         device_id = i % num_devices
-        p = mp.Process(target=worker_init, args=(i, file_chunk, root_folder, device_id, result_queue))
+        p = mp.Process(target=process_files, args=(i, file_chunk, root_folder, device_id, return_dict))
         p.start()
         processes.append(p)
     
-    speaker_dict = {}
-    for p in processes:
-        result = result_queue.get()
-        speaker_dict.update(result)
     for p in processes:
         p.join()
+
+    speaker_dict = {}
+    for rank, part_dict in return_dict.items():
+        speaker_dict.update(part_dict)
+    
     return speaker_dict
 
-def generate_list_dict_from_list(filelist_train, filelist_val, root_folder, num_processes, num_devices):
+def generate_list_dict_from_list(filelist_train, filelist_val, root_folder, num_processes):
     speaker_dict = {'train': {}, 'valid': {}}
-    speaker_dict['valid'] = parallel_process(filelist_val, root_folder, num_processes, num_devices)
-    speaker_dict['train'] = parallel_process(filelist_train, root_folder, num_processes, num_devices)
+    speaker_dict['valid'] = parallel_process(filelist_val, root_folder, num_processes)
+    speaker_dict['train'] = parallel_process(filelist_train, root_folder, num_processes)
     return speaker_dict
 
 if __name__ == "__main__":
@@ -86,7 +87,6 @@ if __name__ == "__main__":
     parser.add_argument('-d', '--dataset_dir', type=str, default="dataset_raw")
     parser.add_argument('-o', '--output',      type=str, default='data/spk2info.dict')
     parser.add_argument('-n', '--num_process', type=int, default=8)
-    parser.add_argument('-g', '--num_devices', type=int, default=6)
     args = parser.parse_args()
 
     with open(args.input_train, "r", encoding='utf-8') as file:
@@ -97,7 +97,7 @@ if __name__ == "__main__":
         data = file.readlines()[1:]
     filelist_val = [line.split("\t")[0] for line in data]
 
-    speaker_list_dict = generate_list_dict_from_list(filelist_train, filelist_val, args.dataset_dir, args.num_process, args.num_devices)
+    speaker_list_dict = generate_list_dict_from_list(filelist_train, filelist_val, args.dataset_dir, args.num_process)
 
     del filelist_train
     del filelist_val
